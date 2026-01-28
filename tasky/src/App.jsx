c577758fb30b40c31a4c82f5a6ca57d57ec31a03
Mark Higgins <markhiggins385@gmail.com>
2026-01-28 16:23:18 +0000

    Add task functionality

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Task from './components/Task';
 import React, { useState } from 'react';
 import AddTaskForm from './components/Form';
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
@@ -12,7 +13,15 @@ function App() {
       { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "medium", done: false },
       { id: 3, title: "Tidy up", deadline: "Today", priority: "low", done: false }
     ]
+
   });
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
+
+
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
@@ -24,6 +33,41 @@ function App() {
     tasks.splice(taskIndex, 1);
     setTaskState({tasks});
   } 
+
+    const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+  }
+
+    console.log(formState);
+
+    const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    
+    tasks.push(form);
+    setTaskState({tasks});
+  }
+  
+
 
 
     return (
@@ -42,7 +86,10 @@ function App() {
 
     />
   ))} 
-<AddTaskForm />
+    <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+
+
+
     </div>
   );
 
